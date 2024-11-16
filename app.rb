e902a020679b501a24d8ef8818ab0b3e538541e1
Handamier Mieraizizi <ezizmir9@gmail.com>
2024-11-16 05:22:53 +0000

    made 	Square with Form feature

--- app.rb
+++ app.rb
@@ -1,9 +1,38 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/:square/results") do
+  @input_num = params.fetch("number").to_i
+  @num_square = (@input_num ** 2).to_f
+
+  erb(:square_results)
+
+end
+
+get("/square_root/new") do
+
+  erb(:square_root_new)
+end
+
+get("/:square_root/results") do
+  @number = params.fetch("user_number").to_i
+  @square_root = Math.sqrt(@number)
+end
+
+get("/random/new") do
+  erb(:index)
+end
+
+get("/payment/new") do
+  erb(:index)
 end
