5c6f8d7f1e4d4f93e023ddd62e191749030c4435
Handamier Mieraizizi <ezizmir9@gmail.com>
2024-11-16 07:57:00 +0000

    made payment with form feature

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ get("/square_root/new") do
   erb(:square_root_new)
 end
 
+
 get("/square_root/results") do
   @number = params.fetch("user_number").to_i
   @square_root = Math.sqrt(@number).to_f
@@ -31,10 +32,28 @@ get("/square_root/results") do
   erb(:square_root_results)
 end
 
-get("/random/new") do
-  erb(:index)
-end
 
 get("/payment/new") do
-  erb(:index)
+
+  erb(:payment_new)
 end
+
+get ("/payment/results") do
+  @user_apr = params.fetch("user_apr").to_f.to_fs(:percentage, {:precision => 4})
+  @user_years = params.fetch("user_years").to_i
+  @user_pv = params.fetch("user_pv").to_f.to_fs(:currency)
+  r = params.fetch("user_apr").to_f / 100 / 12
+  n = params.fetch("user_years").to_i * 12
+  result = (r * @user_years) / (1 - (1 + r) ** -n)
+  @rounded_result = result.to_fs(:currency, {:precision => 2})
+
+  erb(:payment_results)
+end
+
+
+get("/random/new") do
+
+  erb(:random_new)
+end
+
+get()
